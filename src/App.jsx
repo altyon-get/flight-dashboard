ebdddb0c866465c5682cf9cf1ded27d54226afd5
anurag <anuragdw0710@gmail.com>
2024-05-26 22:59:27 +0530

    hide the slow loading when data load

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import Grid from "./components/Grid";
 import Controller from "./components/Controller";
 import InformationBar from "./components/InformationBar";
 import "./App.css";
+import { useSocket } from "./context/SocketContext";
 
 function App() {
+  const { cords } = useSocket();
+  console.log(cords);
   return (
     <>
-      <InformationBar />
+      {cords.length === 0 && <InformationBar />}
       {/* <header>Flight Navigation System</header> */}
       <div className="main-container">
         <div className="grid-container">
